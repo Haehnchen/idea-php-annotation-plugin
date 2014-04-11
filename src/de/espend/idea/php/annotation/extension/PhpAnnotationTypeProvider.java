6f237c00fe63300217671cbe1b797b09ef38bb5b
Daniel Espendiller <espendiller@gmx.de>
2014-04-11 19:13:56 +0200

    add doctrine @Enum value completion

--- src/de/espend/idea/php/annotation/extension/PhpAnnotationTypeProvider.java
+++ src/de/espend/idea/php/annotation/extension/PhpAnnotationTypeProvider.java
@@ -1,15 +1,24 @@
 package de.espend.idea.php.annotation.extension;
 
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag;
 import com.jetbrains.php.lang.psi.elements.Field;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
 import de.espend.idea.php.annotation.AnnotationPropertyParameter;
 import de.espend.idea.php.annotation.PhpAnnotationExtension;
 import de.espend.idea.php.annotation.completion.parameter.CompletionParameter;
+import de.espend.idea.php.annotation.dict.AnnotationTarget;
 import de.espend.idea.php.annotation.reference.parameter.ReferencesByElementParameter;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class PhpAnnotationTypeProvider implements PhpAnnotationExtension {
@@ -24,15 +33,37 @@ public class PhpAnnotationTypeProvider implements PhpAnnotationExtension {
             return null;
         }
 
+        Set<String> values = new HashSet<String>();
         for(Field field: annotationPropertyParameter.getPhpClass().getFields()) {
             if(field.getName().equals(propertyName)) {
+
                 if(field.getType().toString().equals("bool")) {
-                    return Arrays.asList("false", "true");
+                    values.addAll(Arrays.asList("false", "true"));
                 }
+
+                PhpDocComment docComment = field.getDocComment();
+                if(docComment != null) {
+                    PhpDocTag[] phpDocTags = docComment.getTagElementsByName("@Enum");
+                    for(PhpDocTag phpDocTag: phpDocTags) {
+                        PhpPsiElement phpDocAttrList = phpDocTag.getFirstPsiChild();
+                        if(phpDocAttrList != null) {
+                            String enumArrayString = phpDocAttrList.getText();
+                            Pattern targetPattern = Pattern.compile("\"(\\w+)\"");
+
+                            Matcher matcher = targetPattern.matcher(enumArrayString);
+                            while (matcher.find()) {
+                                values.add(matcher.group(1));
+                             }
+
+                        }
+
+                    }
+                }
+
             }
         }
 
-        return null;
+        return values;
     }
 
     @Nullable
