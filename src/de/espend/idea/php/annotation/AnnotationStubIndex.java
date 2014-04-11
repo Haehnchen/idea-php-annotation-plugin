3ab3935c3636c1af8623cb41a8617186b132a576
Daniel Espendiller <espendiller@gmx.de>
2014-04-11 18:32:00 +0200

    remove debug line

--- src/de/espend/idea/php/annotation/AnnotationStubIndex.java
+++ src/de/espend/idea/php/annotation/AnnotationStubIndex.java
@@ -66,7 +66,6 @@ public class AnnotationStubIndex extends FileBasedIndexExtension<String, Void> {
                         // we are on index process, project is not fully loaded here, so filter name based tests
                         // eg PhpUnitUtil.isTestClass not possible
                         if(!fqn.contains("\\Tests\\") && !fqn.contains("\\Fixtures\\") && AnnotationUtil.isAnnotationClass(phpClass)) {
-                            System.out.println(fqn);
                             map.put(phpClass.getPresentableFQN(), null);
                             return;
                         }
