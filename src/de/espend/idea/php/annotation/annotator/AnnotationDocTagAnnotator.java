04591c9b0b5eb2f1be5804d2d33fc13700974b97
Daniel Espendiller <espendiller@gmx.de>
2014-09-29 18:26:16 +0200

    add whitelist for annotation PhpDoc on next siblings #24

--- src/de/espend/idea/php/annotation/annotator/AnnotationDocTagAnnotator.java
+++ src/de/espend/idea/php/annotation/annotator/AnnotationDocTagAnnotator.java
@@ -3,7 +3,11 @@ package de.espend.idea.php.annotation.annotator;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag;
+import com.jetbrains.php.lang.parser.PhpElementTypes;
+import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import de.espend.idea.php.annotation.extension.PhpAnnotationDocTagAnnotator;
 import de.espend.idea.php.annotation.extension.parameter.PhpAnnotationDocTagAnnotatorParameter;
@@ -21,6 +25,16 @@ public class AnnotationDocTagAnnotator implements Annotator {
 
         String name = ((PhpDocTag) psiElement).getName();
         if(AnnotationUtil.NON_ANNOTATION_TAGS.contains(name)) {
+            return;
+        }
+
+        PhpDocComment phpDocComment = PsiTreeUtil.getParentOfType(psiElement, PhpDocComment.class);
+        if(phpDocComment == null) {
+            return;
+        }
+
+        PsiElement nextPsiElement = phpDocComment.getNextPsiSibling();
+        if(nextPsiElement == null || !(nextPsiElement instanceof Method || nextPsiElement instanceof PhpClass || nextPsiElement.getNode().getElementType() == PhpElementTypes.CLASS_FIELDS)) {
             return;
         }
 
