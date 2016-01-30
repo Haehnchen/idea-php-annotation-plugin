ad1134424afded63facbe8df2d98e2ac90d11a25
Daniel Espendiller <espendiller@gmx.de>
2016-01-30 12:59:45 +0100

    add completion for toolbox signature types

---
+++ src/de/espend/idea/php/annotation/toolbox/AnnotationToolboxJsonFileCompletion.java
@@ -0,0 +1,17 @@
+package de.espend.idea.php.annotation.toolbox;
+
+import de.espend.idea.php.toolbox.completion.dict.ToolboxJsonFileCompletionArguments;
+import de.espend.idea.php.toolbox.extension.ToolboxJsonFileCompletion;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Daniel Espendiller <daniel@espendiller.net>
+ */
+public class AnnotationToolboxJsonFileCompletion implements ToolboxJsonFileCompletion {
+    @Override
+    public void addCompletions(@NotNull ToolboxJsonFileCompletionArguments arguments) {
+        if(arguments.getType().equals(ToolboxJsonFileCompletionArguments.TYPE.SIGNATURE_TYPE)) {
+            arguments.addLookupString("annotation");
+        }
+    }
+}
