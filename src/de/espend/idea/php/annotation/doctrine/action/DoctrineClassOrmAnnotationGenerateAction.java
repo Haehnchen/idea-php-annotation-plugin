5506e174b1c95d5de1461445debf14a72c05d0c7
Daniel Espendiller <espendiller@gmx.de>
2016-04-09 19:47:34 +0200

    Doctrine orm class initialize should add an use alias

--- src/de/espend/idea/php/annotation/doctrine/action/DoctrineClassOrmAnnotationGenerateAction.java
+++ src/de/espend/idea/php/annotation/doctrine/action/DoctrineClassOrmAnnotationGenerateAction.java
@@ -5,16 +5,20 @@ import com.intellij.codeInsight.actions.CodeInsightAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.codeInsight.PhpCodeInsightUtil;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
 import com.jetbrains.php.lang.psi.PhpFile;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
 import de.espend.idea.php.annotation.dict.PhpDocCommentAnnotation;
 import de.espend.idea.php.annotation.doctrine.util.DoctrineUtil;
 import de.espend.idea.php.annotation.util.AnnotationUtil;
 import de.espend.idea.php.annotation.util.PhpDocUtil;
+import de.espend.idea.php.annotation.util.PhpElementsUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -87,8 +91,14 @@ public class DoctrineClassOrmAnnotationGenerateAction extends CodeInsightAction 
                     return;
                 }
 
-                PhpDocUtil.addClassOrmDocs(phpClass, editor.getDocument(), file);
+                // insert ORM alias
+                PhpPsiElement scopeForUseOperator = PhpCodeInsightUtil.findScopeForUseOperator(phpClass.getFirstChild());
+                if(scopeForUseOperator != null) {
+                    PhpElementsUtil.insertUseIfNecessary(scopeForUseOperator, DoctrineUtil.DOCTRINE_ORM_MAPPING, "ORM");
+                    PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(editor.getDocument());
+                }
 
+                PhpDocUtil.addClassOrmDocs(phpClass, editor.getDocument(), file);
             }
 
             @Override
