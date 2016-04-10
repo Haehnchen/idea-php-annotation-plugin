55eb121762fddff7f5f4db551bbbb36ff55dfd5f
Daniel Espendiller <espendiller@gmx.de>
2016-04-10 09:48:14 +0200

    reset application settings after test

--- tests/de/espend/idea/php/annotation/tests/completion/AnnotationCompletionContributorTest.java
+++ tests/de/espend/idea/php/annotation/tests/completion/AnnotationCompletionContributorTest.java
@@ -160,6 +160,8 @@ public class AnnotationCompletionContributorTest extends AnnotationLightCodeInsi
                 }
             }
         );
+
+        ApplicationSettings.getInstance().appendRoundBracket = true;
     }
 
     public void testThatAnnotationCompletionInsertUseAlias() {
