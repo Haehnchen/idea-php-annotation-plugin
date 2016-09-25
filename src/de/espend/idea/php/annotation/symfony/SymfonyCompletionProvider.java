7adefea86c34d13586636a1436b3bf57fdae694c
Daniel Espendiller <espendiller@gmx.de>
2016-09-25 14:14:38 +0200

    add Symfony internal route array completion provider #62

---
+++ src/de/espend/idea/php/annotation/symfony/SymfonyCompletionProvider.java
@@ -0,0 +1,26 @@
+package de.espend.idea.php.annotation.symfony;
+
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.jetbrains.php.lang.PhpLangUtil;
+import de.espend.idea.php.annotation.extension.PhpAnnotationCompletionProvider;
+import de.espend.idea.php.annotation.extension.parameter.AnnotationCompletionProviderParameter;
+import de.espend.idea.php.annotation.extension.parameter.AnnotationPropertyParameter;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author Daniel Espendiller <daniel@espendiller.net>
+ */
+public class SymfonyCompletionProvider implements PhpAnnotationCompletionProvider {
+    @Override
+    public void getPropertyValueCompletions(AnnotationPropertyParameter parameter, AnnotationCompletionProviderParameter completion) {
+        if(parameter.getType() != AnnotationPropertyParameter.Type.PROPERTY_ARRAY) {
+            return;
+        }
+
+        if("methods".equals(parameter.getPropertyName()) && PhpLangUtil.equalsClassNames(StringUtils.stripStart(parameter.getPhpClass().getFQN(), "\\"), "Symfony\\Component\\Routing\\Annotation\\Route")) {
+            for (String s : new String[]{"HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "PURGE", "OPTIONS", "TRACE", "CONNECT"}) {
+                completion.getResult().addElement(LookupElementBuilder.create(s));
+            }
+        }
+    }
+}
---
+++ tests/de/espend/idea/php/annotation/tests/symfony/fixtures/classes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Symfony\Component\Routing\Annotation
+{
+    /**
+     * @Annotation
+     */
+    class Route
+    {
+    }
+}
