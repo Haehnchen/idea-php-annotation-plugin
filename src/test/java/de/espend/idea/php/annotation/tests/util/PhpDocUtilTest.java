5f7cfefb7f27ec5d4604700acbac99cf5880d7b9
Daniel Espendiller <daniel@espendiller.net>
2020-05-29 17:00:33 +0200

    prepare constant namespace references support; with pattern checks inside PhpDoc

--- src/test/java/de/espend/idea/php/annotation/tests/util/PhpDocUtilTest.java
+++ src/test/java/de/espend/idea/php/annotation/tests/util/PhpDocUtilTest.java
@@ -14,18 +14,49 @@ public class PhpDocUtilTest extends AnnotationLightCodeInsightFixtureTestCase {
      * @see de.espend.idea.php.annotation.util.PhpDocUtil#getNamespaceForDocIdentifier
      */
     public void testNamespaceIsExtractOnStopChar() {
-        assertEquals("Foo\\Kernel", getPsiElement("@DateTime(Foo\\Ker<caret>nel::VERSION)"));
-        assertEquals("\\Foo\\Kernel", getPsiElement("@DateTime(\\Foo\\Ker<caret>nel::VERSION)"));
-        assertEquals("\\Foo\\Kernel", getPsiElement("@DateTime(\\Foo\\Ker<caret>nel::VERSION)"));
-        assertEquals("Foo\\Kernel", getPsiElement("@DateTime(foo=Foo\\Ker<caret>nel::VERSION)"));
-        assertEquals("Foo\\Kernel", getPsiElement("@DateTime(foo={Foo\\Ker<caret>nel::VERSION)"));
-        assertEquals("Foo\\Kernel", getPsiElement("@DateTime({Foo\\Ker<caret>nel::VERSION)"));
-        assertEquals("Foo\\Kernel", getPsiElement("@DateTime(\"Foo\\Ker<caret>nel::VERSION)"));
-        assertEquals("Kernel", getPsiElement("@DateTime(Ker<caret>nel::VERSION)"));
-        assertEquals("\\Foo\\Kernel", getPsiElement("@DateTime({\\Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("Foo\\Kernel", getLeftPsiElementString("@DateTime(Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("\\Foo\\Kernel", getLeftPsiElementString("@DateTime(\\Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("\\Foo\\Kernel", getLeftPsiElementString("@DateTime(\\Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("Foo\\Kernel", getLeftPsiElementString("@DateTime(foo=Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("Foo\\Kernel", getLeftPsiElementString("@DateTime(foo={Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("Foo\\Kernel", getLeftPsiElementString("@DateTime({Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("Foo\\Kernel", getLeftPsiElementString("@DateTime(\"Foo\\Ker<caret>nel::VERSION)"));
+        assertEquals("Kernel", getLeftPsiElementString("@DateTime(Ker<caret>nel::VERSION)"));
+        assertEquals("\\Foo\\Kernel", getLeftPsiElementString("@DateTime({\\Foo\\Ker<caret>nel::VERSION)"));
     }
 
-    private String getPsiElement(@NotNull String content) {
+    /**
+     * @see de.espend.idea.php.annotation.util.PhpDocUtil#getNamespaceForDocIdentifierAtStart
+     */
+    public void testNamespaceIsExtractOnEndChar() {
+        assertEquals("Foo\\Kernel", getRightPsiElementString("@DateTime(Fo<caret>o\\Kernel::VERSION)"));
+        assertEquals("\\Foo\\Kernel", getRightPsiElementString("@DateTime(\\Fo<caret>o\\Kernel::VERSION)"));
+        assertEquals("Foo\\Kernel", getRightPsiElementString("@DateTime(foo=Fo<caret>o\\Kernel::VERSION)"));
+        assertEquals("Foo\\Kernel", getRightPsiElementString("@DateTime(foo={F<caret>oo\\Kernel::VERSION)"));
+        assertEquals("\\Foo\\Kernel", getRightPsiElementString("@DateTime(foo={\\F<caret>oo\\Kernel::VERSION)"));
+        assertEquals("Foo\\Kernel", getRightPsiElementString("@DateTime({F<caret>oo\\Kernel::VERSION)"));
+        assertEquals("Foo\\Kernel", getRightPsiElementString("@DateTime(\"F<caret>oo\\Kernel::VERSION)"));
+        assertEquals("Kernel", getRightPsiElementString("@DateTime(Ker<caret>nel::VERSION)"));
+    }
+
+    /**
+     * @see de.espend.idea.php.annotation.util.PhpDocUtil#isFirstIdentifierInNamespace
+     */
+    public void testThatIdentifierIsFirstElement() {
+        assertTrue(getIsFirstIdentifierInNamespace("@DateTime(Fo<caret>o\\Kernel::VERSION)"));
+        assertTrue(getIsFirstIdentifierInNamespace("@DateTime(\\Fo<caret>o\\Kernel::VERSION)"));
+        assertTrue(getIsFirstIdentifierInNamespace("@DateTime(foo=Fo<caret>o\\Kernel::VERSION)"));
+        assertTrue(getIsFirstIdentifierInNamespace("@DateTime(foo={\\F<caret>oo\\Kernel::VERSION)"));
+        assertTrue(getIsFirstIdentifierInNamespace("@DateTime({F<caret>oo\\Kernel::VERSION)"));
+        assertTrue(getIsFirstIdentifierInNamespace("@DateTime(\"F<caret>oo\\Kernel::VERSION)"));
+        assertTrue(getIsFirstIdentifierInNamespace("@DateTime(Ker<caret>nel::VERSION)"));
+
+        assertFalse(getIsFirstIdentifierInNamespace("@DateTime(\\Kernel\\Fo<caret>o::VERSION)"));
+        assertFalse(getIsFirstIdentifierInNamespace("@DateTime(foo=Foo\\Kern<caret>el::VERSION)"));
+        assertFalse(getIsFirstIdentifierInNamespace("@DateTime(foo={\\Foo\\Ker<caret>nel::VERSION)"));
+    }
+
+    private String getLeftPsiElementString(@NotNull String content) {
         myFixture.configureByText(PhpFileType.INSTANCE, "<?php" +
             "/**\n" +
             " * " + content + "\n" +
@@ -33,6 +64,30 @@ public class PhpDocUtilTest extends AnnotationLightCodeInsightFixtureTestCase {
         );
 
         return PhpDocUtil.getNamespaceForDocIdentifier(
+            myFixture.getFile().findElementAt(myFixture.getCaretOffset())
+        );
+    }
+
+    private String getRightPsiElementString(@NotNull String content) {
+        myFixture.configureByText(PhpFileType.INSTANCE, "<?php" +
+            "/**\n" +
+            " * " + content + "\n" +
+            "*/"
+        );
+
+        return PhpDocUtil.getNamespaceForDocIdentifierAtStart(
+            myFixture.getFile().findElementAt(myFixture.getCaretOffset())
+        );
+    }
+
+    private boolean getIsFirstIdentifierInNamespace(@NotNull String content) {
+        myFixture.configureByText(PhpFileType.INSTANCE, "<?php" +
+            "/**\n" +
+            " * " + content + "\n" +
+            "*/"
+        );
+
+        return PhpDocUtil.isFirstIdentifierInNamespace(
             myFixture.getFile().findElementAt(myFixture.getCaretOffset())
         );
     }
