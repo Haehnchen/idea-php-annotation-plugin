b0c758804af4f9a79a434534717cb0c10a175bcc
Daniel Espendiller <espendiller@gmx.de>
2020-05-29 17:05:31 +0200

    move custom import alias to internal util class and filter by active state

--- src/main/java/de/espend/idea/php/annotation/util/AnnotationUtil.java
+++ src/main/java/de/espend/idea/php/annotation/util/AnnotationUtil.java
@@ -13,6 +13,7 @@ import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Processor;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.FileBasedIndex;
 import com.intellij.util.indexing.FileContent;
 import com.intellij.util.indexing.ID;
@@ -454,7 +455,7 @@ public class AnnotationUtil {
         if (split.length > 1) {
             String aliasStart = split[0];
 
-            for (UseAliasOption useAliasOption : ApplicationSettings.getUseAliasOptionsWithDefaultFallback()) {
+            for (UseAliasOption useAliasOption : AnnotationUtil.getActiveImportsAliasesFromSettings()) {
                 String alias = useAliasOption.getAlias();
 
                 if (!aliasStart.equals(alias)) {
@@ -657,6 +658,16 @@ public class AnnotationUtil {
         }
 
         return psiElements;
+    }
+
+    @NotNull
+    public static Collection<UseAliasOption> getActiveImportsAliasesFromSettings() {
+        Collection<UseAliasOption> useAliasOptions = ApplicationSettings.getUseAliasOptionsWithDefaultFallback();
+        if(useAliasOptions.size() == 0) {
+            return Collections.emptyList();
+        }
+
+        return ContainerUtil.filter(useAliasOptions, UseAliasOption::isEnabled);
     }
 
     /**
